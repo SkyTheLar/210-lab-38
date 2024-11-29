141aec577ef90b50016e58bebcd6c6350cfcad2a
Skylar Robinson <skylardawn150@gmail.com>
2024-11-29 14:47:19 -0800

    Commit 1

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/**************************************************
+
+COMSC 210 | Lab 38 | Skylar Robinson | IDE Used: Eclipse
+
+**************************************************/
+
+#include <iostream>
+#include "IntBinaryTree.h"
+using namespace std;
+
+int main() {
+    
+
+    return 0;
+}
