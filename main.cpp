921d10eacc97681354f9b81b0ae095a9f0f7f24e
Skylar Robinson <skylardawn150@gmail.com>
2024-11-29 15:20:49 -0800

    Commit 4

--- main.cpp
+++ main.cpp
@@ -11,6 +11,10 @@ COMSC 210 | Lab 38 | Skylar Robinson | IDE Used: Eclipse
 using namespace std;
 
 int menu();
+void addCode(BinaryTree&);
+void delCode(BinaryTree&);
+void findCode(BinaryTree);
+void changeCode(BinaryTree&);
 
 int main() {
 	BinaryTree tree;
@@ -29,19 +33,19 @@ int main() {
     	tree.insertNode(code);
     }
 
-    tree.displayInOrder();
-
     //close file
     in.close();
 
     int choice = menu();
     while (choice != 5) {
-    	switch (choice)
-    	case 1: break;
-    	case 2: break;
+    	switch (choice) {
+    	case 1: addCode(tree); break;
+    	case 2: delCode(tree); break;
     	case 3: break;
     	case 4: break;
     	default: break;
+    	}
+    	choice = menu();
     }
 
     return 0;
@@ -55,9 +59,33 @@ int menu() {
 		 << "[2] Delete code\n"
 		 << "[3] Search for code\n"
 		 << "[4] Modify code\n"
-		 << "[5] Exit"
+		 << "[5] Exit\n"
 		 << "Enter a choice by number-> ";
 	cin >> choice;
 	cin.ignore();
 	return choice;
+}
+
+void addCode(BinaryTree& t) {
+	string temp;
+	cout << "Enter the code to add: ";
+	getline(cin, temp);
+	t.insertNode(temp);
+	cout << temp << " inserted!\n";
+}
+
+void delCode(BinaryTree& t) {
+	string temp;
+	cout << "Enter the code to delete: ";
+	getline(cin, temp);
+	t.remove(temp);
+	cout << temp << " is no longer in records!\n";
+}
+
+void findCode(BinaryTree) {
+
+}
+
+void changeCode(BinaryTree&) {
+
 }
